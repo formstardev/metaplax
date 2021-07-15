20b476dc8a838c2c5db8c7e285ba36478ad1272d
WonderfulDev <abandonraj@gmail.com>
2021-07-15 16:30:38 -0500

    styling navbar

--- js/packages/web/src/views/auctionCreate/artSelector.tsx
+++ js/packages/web/src/views/auctionCreate/artSelector.tsx
@@ -98,10 +98,7 @@ export const ArtSelector = (props: ArtSelectorProps) => {
             Select the NFT that you want to sell copy/copies of.
           </p>
         </Row>
-        <Row
-          className="content-action"
-          style={{ overflowY: 'auto', height: '50vh' }}
-        >
+        <Row className="content-action" style={{ overflowY: 'auto', height: "50vh" }}>
           <Masonry
             breakpointCols={breakpointColumnsObj}
             className="my-masonry-grid"
