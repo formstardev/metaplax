9ae14e81bc01029ccaf73f4b81f21c33a3b19bbc
WonderfulDev <abandonraj@gmail.com>
2021-07-15 15:43:49 -0500

    connect wallet btn

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -8,21 +8,6 @@ import useWindowDimensions from '../../utils/layout';
 import { MenuOutlined } from '@ant-design/icons';
 import { useMeta } from '../../contexts';
 
-const getDefaultLinkActions = (connected: boolean) => {
-  return [
-    <Link to={`/artworks`}>
-      <Button className="app-btn">{connected ? "My Items" : "Artworks"}</Button>
-    </Link>,
-    <Link to={`/artists`}>
-      <Button className="app-btn">Creators</Button>
-    </Link>,
-    <Link to={`/artistAlley`}>
-      <Button className="app-btn">Artist Alley</Button>
-    </Link>
-  ]
-}
-
-
 const UserActions = () => {
   const { wallet } = useWallet();
   const { whitelistedCreatorsByCreator, store } = useMeta();
@@ -58,7 +43,18 @@ const DefaultActions = ({ vertical = false }: { vertical?: boolean }) => {
       display: "flex",
       flexDirection: vertical ? "column" : "row",
     }}>
-      {getDefaultLinkActions(connected)}
+      <Link to={`/`}>
+        <Button className="app-btn">Explore</Button>
+      </Link>
+      <Link to={`/artworks`}>
+        <Button className="app-btn">{connected ? "My Items" : "Artworks"}</Button>
+      </Link>
+      <Link to={`/artists`}>
+        <Button className="app-btn">Creators</Button>
+      </Link>
+      <Link to={`/artistAlley`}>
+        <Button className="app-btn">Artist Alley</Button>
+      </Link>
     </div>
   )
 }
@@ -73,13 +69,21 @@ const MetaplexMenu = () => {
       placement="bottomLeft"
       trigger={['click']}
       overlay={<Menu>
-        {getDefaultLinkActions(connected).map(item => {
-          return (
-            <Menu.Item>
-              {item}
-            </Menu.Item>
-          )
-        })}
+        <Menu.Item>
+          <Link to={`/`}>
+            <Button className="app-btn">Explore</Button>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to={`/artworks`}>
+            <Button className="app-btn">{connected ? "My Items" : "Artworks"}</Button>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to={`/artists`}>
+            <Button className="app-btn">Creators</Button>
+          </Link>
+        </Menu.Item>
       </Menu>}
     >
       <MenuOutlined style={{ fontSize: "1.4rem" }} />
@@ -89,23 +93,14 @@ const MetaplexMenu = () => {
   return <DefaultActions />
 }
 
-export const LogoLink = () => {
-  return  (
-    <Link to={`/`}>
-      <img src={'/mcfarlane-logo.svg'} />
-    </Link>
-  )
-}
-
 export const AppBar = () => {
   const { connected } = useWallet();
 
   return (
     <>
       <div className="app-left app-bar-box">
-        <LogoLink />
-        <div className="divider" />
         <Notifications />
+        <div className="divider" />
         <MetaplexMenu />
       </div>
       {!connected && <ConnectButton type="primary" />}
