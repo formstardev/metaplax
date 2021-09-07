f2b2aa376a238bb066378fcdb4262787edd192e1
WonderfulDev <abandonraj@gmail.com>
2021-09-07 17:08:41 -0500

    Finished asset

--- js/packages/web/src/views/preLaunch/index.tsx
+++ js/packages/web/src/views/preLaunch/index.tsx
@@ -1,11 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, {useCallback, useState} from 'react'
 
-import { Button, Form, Input, Layout, Modal } from "antd";
+import {Button, Input, Layout, Modal} from "antd";
 import { ModalProps } from 'antd/lib/modal';
 import { LogoLink } from "../../components/AppBar";
 import { textContent } from "./textContent";
-import useMagicLink from "../../hooks/magicLink/useMagicLink";
-
 const { Content } = Layout;
 
 export interface GotEmailButtonProps
@@ -16,6 +14,13 @@ export interface GotEmailButtonProps
   visible?: boolean;
   extraButton?: JSX.Element,
 }
+
+const DiscordButton = () => (
+  <a className={"discord-button"} target={"_blank"} href={"https://discord.com/invite/metaplex"}>
+    <span></span> Join our Discord
+  </a>
+)
+
 
 const PreLaunchModal = (props: GotEmailButtonProps) => {
   const { onCancel, visible, titleText, descriptionText, extraButton, className, ...rest } = props;
@@ -43,7 +48,7 @@ const PreLaunchModal = (props: GotEmailButtonProps) => {
         {descriptionText}
       </div>
       {extraButton && (
-        { extraButton }
+        extraButton
       )}
     </Modal>
   );
@@ -56,39 +61,31 @@ export const PreLaunchView = () => {
   const [submitted, setSubmitted] = useState(false)
   const [gotVisible, setGotVisible] = useState(false)
   const [sentVisible, setSentVisible] = useState(false)
-  const auth = useMagicLink()
-  const verifyUser = async () => {
-    if (auth.loggedIn) {
-      const user = await auth.magic.user.getMetadata();
-      setGotVisible(false)
-      setEmail(user.email)
-      setVerified(true)
-    }
-  }
-  const saveTypeForm = async () => {
-    setSubmitted(true)
 
-
-  }
-  useEffect(() => {
-    verifyUser()
-  }, [auth.loggedIn])
   return (
     <Layout id={'pre-launch-layout'}>
+      <div className={"main-asset-banner"}>
+        {/*Gradient does not match*/}
+        {/*<div className={"right-gradient"}></div>*/}
+      </div>
       <PreLaunchModal
         titleText={textContent.gotEmail}
         descriptionText={textContent.gotEmailDescription}
         visible={gotVisible}
         onCancel={() => {
           setGotVisible(false);
-          //setVerified(true) //remove later
+          setVerified(true) //remove later
         }}
       />
       <PreLaunchModal
         titleText={textContent.sentNFT}
         descriptionText={textContent.sentNFTDescription}
         visible={sentVisible}
-        onCancel={() => setSentVisible(false)}
+        onCancel={() => {
+          setSentVisible(false)
+          setSubmitted(true)
+        }}
+        extraButton={<DiscordButton />}
       />
       <Layout id={'width-layout'}>
         {!verified ? (
@@ -97,7 +94,7 @@ export const PreLaunchView = () => {
           >
             <div className={"upper-content"}>
               <div className={"logo"}>
-                <LogoLink/>
+                <LogoLink />
               </div>
               <div className={"pre-title"}>
                 {textContent.mainTitle}
@@ -106,31 +103,8 @@ export const PreLaunchView = () => {
                 {textContent.titleDescription}
               </div>
               <div className={"pre-input"}>
-                <Form
-                  className={'footer-sign-up'}
-                  onFinish={(values) => {
-                    auth.login(values.email)
-                    setGotVisible(true)
-                  }}
-                >
-                  <Form.Item
-                    name='email'
-                    rules={[
-                      {
-                        type: 'email', message: 'Input is not a valid email!'
-                      },
-                      { required: true, message: 'Please input your email!' }
-                    ]}
-                    style={{ display: 'flex !important' }}
-                  >
-                    <Input
-                      className={'footer-input'}
-                      placeholder="Email Address"
-                      name="email"
-                    />
-                  </Form.Item>
-                  <Button className={"secondary-btn sign-up"} htmlType="submit">Sign Up</Button>
-                </Form>
+                <Input value={email} placeholder={"Email"} onChange={(val) => setEmail(val.target.value)} />
+                <Button className={"secondary-btn sign-up"} onClick={() => setGotVisible(true)}>Sign Up</Button>
               </div>
             </div>
             <div className={"lower-content"}>
@@ -156,16 +130,49 @@ export const PreLaunchView = () => {
             className={"pre-main-content second"}
           >
             <div className={"logo"}>
-              <LogoLink/>
+              <LogoLink />
             </div>
             <div className={"verify-message"}>
               <span>Thanks for verifying</span>
               <span className={"email"}>{email}</span>
             </div>
+            <div className={"verify-message mb32"}>
+              <span>Paste your Solana wallet address here.</span>
+            </div>
             <div className={"pre-input wallet"}>
               <Input value={walletAddress} placeholder={"Wallet address"}
-                     onChange={(val) => setWalletAddress(val.target.value)}/>
-              <Button className={"secondary-btn sign-up"} onClick={() => saveTypeForm()}>Submit</Button>
+                     onChange={(val) => setWalletAddress(val.target.value)} />
+              <Button className={"secondary-btn sign-up"} onClick={() => setSentVisible(true)}>Submit</Button>
+            </div>
+            <div className={"verify-message mb40"}>
+              <span>How to create a wallet:</span>
+            </div>
+            <div className={"steps"}>
+              <div className={"step"}>
+                <span className={"step-title"}>Step 1</span>
+                <span className={"step-desc"}>Install Phantom.</span>
+                <div className={"step-asset step1"}></div>
+                <span className={"step-desc"}>
+                  Install the Phantom wallet in Google Chrome, Firefox, Brave, or Edge
+                  via Phantom’s website. It’s free to use!
+                </span>
+              </div>
+              <div className={"step"}>
+                <span className={"step-title"}>Step 2</span>
+                <span className={"step-desc"}>Create a new wallet.</span>
+                <div className={"step-asset step2"}></div>
+                <span className={"step-desc"}>Once it’s installed, follow the steps to create a new wallet.</span>
+              </div>
+              <div className={"step"}>
+                <span className={"step-title"}>Step 3</span>
+                <span className={"step-desc"}>Copy your wallet address.</span>
+                <div className={"step-asset step3"}></div>
+                <span className={"step-desc"}>
+                  Once you’re signed in click the top bar to copy your wallet address.
+                  In Phantom it displays a condensed version (ex: CRWJ...ch67),
+                  but when you paste it here, you should see a long string of letters and numbers.
+                </span>
+              </div>
             </div>
           </Content>
         ) : (
@@ -177,4 +184,4 @@ export const PreLaunchView = () => {
       </Layout>
     </Layout>
   );
-}
+};
